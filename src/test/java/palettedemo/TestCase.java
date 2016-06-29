08d9df56f95b68db75cacbb74aac3686662f1556
Devesh Suntha <LENOVO@idea-PC>
2016-06-29 21:42:54 +0530

    Added test case

---
+++ src/test/java/palettedemo/TestCase.java
@@ -0,0 +1,20 @@
+package palettedemo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCase {
+
+	@Test
+	public void test() {
+		assertEquals(2, 1+1);
+	}
+	
+	
+	@Test
+	public void test2() {
+		
+	}
+
+}
